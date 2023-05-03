693b710aafb8c415167e6245d26b77f67419658d
Kaytotes <me@kaytotes.online>
2023-05-03 22:41:07 +0100

    Fix various abilities that should be usable while controlled.
    Will of the Forsaken, Ice Block, Divine Protection, Divine Shield etc

--- src/server/game/Handlers/SpellHandler.cpp
+++ src/server/game/Handlers/SpellHandler.cpp
@@ -345,7 +345,20 @@ void WorldSession::HandleCastSpellOpcode(WorldPacket& recvPacket)
 
     // ignore for remote control state (for player case)
     Unit* mover = GetGameClient()->GetActivelyMovedUnit();
-    if (!mover || (mover != _player && mover->GetTypeId() == TYPEID_PLAYER))
+    /** @epoch-start */
+    if(!mover)
+    {
+        //there is no ActivelyMovedUnit if player is under fear, confuse or possession
+        if(_player->IsFeared() || _player->IsConfused() || _player->isPossessed())
+            mover = _player;
+        else
+        {
+            recvPacket.rfinish(); // prevent spam at ignore packet
+            return;
+        }
+    }
+    else if (mover != _player && mover->GetTypeId() == TYPEID_PLAYER)
+    /** @epoch-end */
     {
         recvPacket.rfinish(); // prevent spam at ignore packet
         return;
