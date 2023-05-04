14253a97c0ed4b80834511dc1901569623ecfd11
Kaytotes <me@kaytotes.online>
2023-05-04 18:49:17 +0100

    Rework Rend Scaling
    Matches TBC now

--- src/server/scripts/Spells/spell_warrior.cpp
+++ src/server/scripts/Spells/spell_warrior.cpp
@@ -579,6 +579,7 @@ class spell_warr_rend : public AuraScript
         {
             canBeRecalculated = false;
 
+            /** @epoch-start */
             // $0.2 * (($MWB + $mwb) / 2 + $AP / 14 * $MWS) bonus per tick
             float ap = caster->GetTotalAttackPowerValue(BASE_ATTACK);
             int32 mws = caster->GetAttackTime(BASE_ATTACK);
@@ -590,16 +591,9 @@ class spell_warr_rend : public AuraScript
                 mwbMax += caster->GetWeaponDamageRange(BASE_ATTACK, MAXDAMAGE, i);
             }
 
-            float mwb = ((mwbMin + mwbMax) / 2 + ap * mws / 14000) * 0.2f;
+            float mwb = ((mwbMin + mwbMax) / 2 + ap * mws / 14000) * 0.00743f;
             amount += int32(caster->ApplyEffectModifiers(GetSpellInfo(), aurEff->GetEffIndex(), mwb));
-
-            // "If used while your target is above 75% health, Rend does 35% more damage."
-            // as for 3.1.3 only ranks above 9 (wrong tooltip?)
-            if (GetSpellInfo()->GetRank() >= 9)
-            {
-                if (GetUnitOwner()->HasAuraState(AURA_STATE_HEALTH_ABOVE_75_PERCENT, GetSpellInfo(), caster))
-                    AddPct(amount, GetEffectInfo(EFFECT_2).CalcValue(caster));
-            }
+            /** @epoch-end */
         }
     }
 
