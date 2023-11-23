9472345c402fda40a30965b6b81378105bf70cc4
Kaytotes <me@kaytotes.online>
2023-11-23 06:03:19 +0000

    Pet Mounting
    Pets should no longer despawn when mounting and should instead enter an inactive state.

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -8362,12 +8362,16 @@ void Unit::Mount(uint32 mount, uint32 VehicleId, uint32 creatureEntry)
         Pet* pet = player->GetPet();
         if (pet)
         {
-            Battleground* bg = ToPlayer()->GetBattleground();
-            // don't unsummon pet in arena but SetFlag UNIT_FLAG_STUNNED to disable pet's interface
-            if (bg && bg->isArena())
-                pet->SetUnitFlag(UNIT_FLAG_STUNNED);
-            else
-                player->UnsummonPetTemporaryIfAny();
+            /** @epoch-start */
+            pet->SetUnitFlag(UNIT_FLAG_STUNNED); // disable pet's interface
+
+            // Battleground* bg = ToPlayer()->GetBattleground();
+            // // don't unsummon pet in arena but SetFlag UNIT_FLAG_STUNNED to disable pet's interface
+            // if (bg && bg->isArena())
+            //     pet->SetUnitFlag(UNIT_FLAG_STUNNED);
+            // else
+            //     player->UnsummonPetTemporaryIfAny();
+            /** @epoch-end */
         }
 
         // if we have charmed npc, stun him also (everywhere)
