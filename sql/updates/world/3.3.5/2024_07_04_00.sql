639f89c93491830877f8ef07ebfc943864f89c27
Kaytotes <me@kaytotes.online>
2024-07-04 21:01:00 +0100

    Move Ally GY when dying in Thunder Bluff

---
+++ sql/updates/world/3.3.5/2024_07_04_00.sql
@@ -0,0 +1,7 @@
+-- Deletes Ratchet as Ally GY when dying in Thunder Bluff
+DELETE FROM `graveyard_zone` WHERE `ID`= 249 AND `GhostZone`= 1638;
+
+-- Adds Bloodhoof Village as Ally GY when dying in Thunder Bluff
+DELETE FROM `graveyard_zone` WHERE `ID`= 1435 AND `GhostZone`= 1638;
+INSERT INTO `graveyard_zone` (`ID`, `GhostZone`, `Faction`, `Comment`) VALUES
+(1435, 1638, 469, 'Mulgore, Bloodhoof Village GY - Mulgore');
