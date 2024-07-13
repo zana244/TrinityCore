54fc551923025344b1a7e545d108566f3a023a7e
Kaytotes <me@kaytotes.online>
2024-07-13 03:48:51 +0100

    Merge updates and remove comments. TS-WoW update applier doesnt like them

--- sql/updates/world/3.3.5/2024_07_08_00_world.sql
+++ sql/updates/world/3.3.5/2024_07_08_00_world.sql
@@ -1,1 +1,7 @@
 ALTER TABLE `creature_template` ADD COLUMN `corpse_delay` INT UNSIGNED NOT NULL DEFAULT 300 AFTER `ScriptName`;
+
+UPDATE `creature_template` SET `corpse_delay` = 900 WHERE `rank` = 1;
+UPDATE `creature_template` SET `corpse_delay` = 1200 WHERE `rank` = 2;
+UPDATE `creature_template` SET `corpse_delay` = 3600 WHERE `rank` = 3;
+UPDATE `creature_template` SET `corpse_delay` = 900 WHERE `rank` = 4;
+UPDATE `creature_template` SET `corpse_delay` = 60 WHERE `rank` = 5;
