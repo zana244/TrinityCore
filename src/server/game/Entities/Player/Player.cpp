84400748fb080e585f5eefa8a6431b402a78a0ee
tapmondev <tapmondev@proton.me>
2024-09-29 22:40:09 +0200

    core/player: emit loading info OnCanEquip
    This commit adds the `not_loading` bool to the OnCanEquip event.
    
    (cherry picked from commit cb26dc6af831debf373c9613c4b6d5cee31adf1c)

--- src/server/game/Entities/Player/Player.cpp
+++ src/server/game/Entities/Player/Player.cpp
@@ -11586,6 +11586,7 @@ InventoryResult Player::CanEquipItem(uint8 slot, uint16 &dest, Item* pItem, bool
                 , TSPlayer(const_cast<Player*>(this))
                 , slot
                 , swap
+                , not_loading
                 , TSMutableNumber<uint32>(&evtRes)
             );
             return InventoryResult(evtRes);
