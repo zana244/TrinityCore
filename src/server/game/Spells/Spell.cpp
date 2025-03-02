zana244/TrinityCore

commit 26724ec217e5a8dde91c4369123a43dac240d91e

Properly retrieve pet to dismiss for spells with SPELL_ATTR1_DISMISS_PET. (#88)
For some reason, the previous GetCreature function was not returning a
valid pointer when a warlock would use Enslave Demon with a summoned
pet, leading to the warlock having a pet + a charm. Modified to use the
more restrictive GetPet (since we are expecting a Pet...)

Co-authored-by: zana244 <zana244@users.noreply.github.com>

src/server/game/Spells/Spell.cpp
@@ -3495,7 +3495,7 @@ void Spell::_cast(bool skipCheck)
 
     if (Unit* unitCaster = m_caster->ToUnit())
         if (m_spellInfo->HasAttribute(SPELL_ATTR1_DISMISS_PET))
-            if (Creature* pet = ObjectAccessor::GetCreature(*m_caster, unitCaster->GetPetGUID()))
+            if (Pet* pet = ObjectAccessor::GetPet(*m_caster, unitCaster->GetPetGUID()))
                 pet->DespawnOrUnsummon();
 
     PrepareTriggersExecutedOnHit();

commit d83dafbcc52d08e0832c139a1c7d9db8ce424211

Fix SPELL_ATTR1_DISMISS_PET regression caused by https://github.com/Project-Epoch/TrinityCore/commit/26724ec217e5a8dde91c4369123a43dac240d91e
Previous commit made the attribute work for players and broke it for
NPCs. We handle both cases now.

src/server/game/Spells/Spell.cpp
@@ -3516,9 +3516,15 @@ void Spell::_cast(bool skipCheck)
     }
 
     if (Unit* unitCaster = m_caster->ToUnit())
+    {
         if (m_spellInfo->HasAttribute(SPELL_ATTR1_DISMISS_PET))
-            if (Pet* pet = ObjectAccessor::GetPet(*m_caster, unitCaster->GetPetGUID()))
+        {
+            if (Pet* pet = ObjectAccessor::GetPet(*m_caster, unitCaster->GetPetGUID())) // For Players
                 pet->DespawnOrUnsummon();
+            else if (Creature* pet = ObjectAccessor::GetCreature(*m_caster, unitCaster->GetPetGUID())) // For NPCs
+                pet->DespawnOrUnsummon();
+        }
+    }
 
     PrepareTriggersExecutedOnHit();
 